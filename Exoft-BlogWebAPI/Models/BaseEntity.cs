225df0386db3fd8ddef1135258c16f0a15166ba5
Valentine <mselinkor@gmail.com>
2022-08-18 17:57:39 +0300

    added UpdateTime to BaseEntity

--- Exoft-BlogWebAPI/Models/BaseEntity.cs
+++ Exoft-BlogWebAPI/Models/BaseEntity.cs
@@ -6,5 +6,6 @@
         public Guid CreatedBy { get; set; }
         public Guid UpdatedBy { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
     }
 }
